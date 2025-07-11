0cad6e7ca20974caf129faddeb6ea0f92cf21336
fbogliacino <46468093+fbogliacino@users.noreply.github.com>
2025-07-11 13:00:32 +0200

    lecture 10
    All lectures completed
    only fe py implementation missing

--- analysis/olsFeRegression.py
+++ analysis/olsFeRegression.py
@@ -5,6 +5,11 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+from linearmodels import PanelOLS 
+from fixedeffect import fixedeffect
+
+
+
 # Load the data
 data = pd.read_excel('SimulatedData.xlsx', sheet_name='database')
 print(data.head())
@@ -13,3 +18,13 @@ Y=data.outcome1
 # Fit the OLS regression model
 result = sm.OLS(Y, sm.add_constant(D)).fit()
 print(result.summary())
+
+# Save the results to a text file
+with open('ols_results.txt', 'w') as f:
+    f.write(result.summary().as_text()) 
+
+#now import the new data to estimate with Fixed Effects    
+data_fe = pd.read_excel('panelData.xlsx', sheet_name='panel')
+print(data_fe.head())
+
+# Fit the Fixed Effects regression model
