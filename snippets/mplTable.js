ad5a28d96d0fe9136f0b131a2ecd352a284d8256
fbogliacino <46468093+fbogliacino@users.noreply.github.com>
2025-07-21 10:26:25 -0400

    updated in class

---
+++ snippets/mplTable.js
@@ -0,0 +1,108 @@
+.myDiv{
+  background-color: blueviolet;
+  margin: auto;  
+  width: 900px;
+  text-align: center;
+}
+
+.checkmark {
+          background-color: whitesmoke;
+        }
+
+<div class="myDiv">
+<table>
+  <thead>
+    <tr>
+      <td></td><td><strong>Left option</strong></td><td><strong>Right option</strong></td><td></td>
+    </tr>
+  </thead> 
+      <tr>
+      <td><label class="labelInputs" for="MDG0_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG0_l" name="MPL0" type="radio" value="1"></td><td><input id="MDG0_r" name="MPL0" type="radio" value="2"></td><td><label class="labelInputs" for="MDG0_r"><strong>0</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG1_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG1_l" name="MPL1" type="radio" value="1" ></td><td><input id="MDG1_r" name="MPL1" type="radio" value="2"></td><td><label class="labelInputs" for="MDG1_r"><strong>1</strong> for <strong>You</strong>, <strong>1</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG2_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG2_l" name="MPL2" type="radio" value="1" ></td><td><input id="MDG2_r" name="MPL2" type="radio" value="2"></td><td><label class="labelInputs" for="MDG2_r"><strong>2</strong> for <strong>You</strong>, <strong>2</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG3_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG3_l" name="MPL3" type="radio" value="1" ></td><td><input id="MDG3_r" name="MPL3" type="radio" value="2"></td><td><label class="labelInputs" for="MDG3_r"><strong>3</strong> for <strong>You</strong>, <strong>3</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG4_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG4_l" name="MPL4" type="radio" value="1" ></td><td><input id="MDG4_r" name="MPL4" type="radio" value="2"></td><td><label class="labelInputs" for="MDG4_r"><strong>4</strong> for <strong>You</strong>, <strong>4</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG5_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG5_l" name="MPL5" type="radio" value="1" ></td><td><input id="MDG5_r" name="MPL5" type="radio" value="2"></td><td><label class="labelInputs" for="MDG5_r"><strong>5</strong> for <strong>You</strong>, <strong>5</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG6_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG6_l" name="MPL6" type="radio" value="1" ></td><td><input id="MDG6_r" name="MPL6" type="radio" value="2"></td><td><label class="labelInputs" for="MDG6_r"><strong>6</strong> for <strong>You</strong>, <strong>6</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG7_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG7_l" name="MPL7" type="radio" value="1" ></td><td><input id="MDG7_r" name="MPL7" type="radio" value="2"></td><td><label class="labelInputs" for="MDG7_r"><strong>7</strong> for <strong>You</strong>, <strong>7</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG8_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG8_l" name="MPL8" type="radio" value="1" ></td><td><input id="MDG8_r" name="MPL8" type="radio" value="2"></td><td><label class="labelInputs" for="MDG8_r"><strong>8</strong> for <strong>You</strong>, <strong>8</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG9_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG9_l" name="MPL9" type="radio" value="1" ></td><td><input id="MDG9_r" name="MPL9" type="radio" value="2"></td><td><label class="labelInputs" for="MDG9_r"><strong>9</strong> for <strong>You</strong>, <strong>9</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG10_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG10_l" name="MPL10" type="radio" value="1" ></td><td><input id="MDG10_r" name="MPL10" type="radio" value="2"></td><td><label class="labelInputs" for="MDG10_r"><strong>10</strong> for <strong>You</strong>, <strong>10</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG11_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input required id="MDG11_l" name="MPL11" type="radio" value="1" ></td><td><input id="MDG11_r" name="MPL11" type="radio" value="2"></td><td><label class="labelInputs" for="MDG11_r"><strong>11</strong> for <strong>You</strong>, <strong>11</strong> for <strong>Other</strong></label></td>
+    </tr>
+</table>
+</div>
+
+
+
+
+
+
+
+<div class="myDiv">
+<table>
+  <thead>
+    <tr>
+      <td></td><td><strong>Left option</strong></td><td><strong>Right option</strong></td><td></td>
+    </tr>
+  </thead> 
+      <tr>
+      <td><label class="labelInputs" for="MDG0_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG0_l" class="checkmark" type="radio" value="1"></td><td><input id="MDG0_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG0_r"><strong>0</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG1_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG1_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG1_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG1_r"><strong>1</strong> for <strong>You</strong>, <strong>1</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG2_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG2_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG2_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG2_r"><strong>2</strong> for <strong>You</strong>, <strong>2</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG3_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG3_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG3_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG3_r"><strong>3</strong> for <strong>You</strong>, <strong>3</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG4_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG4_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG4_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG4_r"><strong>4</strong> for <strong>You</strong>, <strong>4</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG5_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG5_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG5_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG5_r"><strong>5</strong> for <strong>You</strong>, <strong>5</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG6_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG6_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG6_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG6_r"><strong>6</strong> for <strong>You</strong>, <strong>6</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG7_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG7_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG7_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG7_r"><strong>7</strong> for <strong>You</strong>, <strong>7</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG8_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG8_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG8_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG8_r"><strong>8</strong> for <strong>You</strong>, <strong>8</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG9_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG9_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG9_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG9_r"><strong>9</strong> for <strong>You</strong>, <strong>9</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG10_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG10_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG10_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG10_r"><strong>10</strong> for <strong>You</strong>, <strong>10</strong> for <strong>Other</strong></label></td>
+    </tr>
+          <tr>
+      <td><label class="labelInputs" for="MDG11_l"><strong>10</strong> for <strong>You</strong>, <strong>0</strong> for <strong>Other</strong></label></td><td><input id="MDG11_l" class="checkmark" type="radio" value="1" ></td><td><input id="MDG11_r" class="checkmark" type="radio" value="2"></td><td><label class="labelInputs" for="MDG11_r"><strong>11</strong> for <strong>You</strong>, <strong>11</strong> for <strong>Other</strong></label></td>
+    </tr>
+</table>
+</div>
